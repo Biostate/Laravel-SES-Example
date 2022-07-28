bf272e8a4d029daed13a848fe30b5c8694d87427
Süleyman Özgür Özarpacı <oozarpac@gmail.com>
2022-07-28 15:50:40 +0300

    basic send and preview endpoints added to route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Mail\TestMail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/send', function () {
+    Mail::to(config('app.test_to_email_address'))->send(new TestMail());
+    return "OK";
+});
+
+Route::get('/email-preview', function () {
+    return (new TestMail())->render();
 });
